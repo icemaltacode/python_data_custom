1d464f8487a2e452ac34faff127c446eb26ffb35
Keith Vassallo <keith@icemalta.com>
2026-06-09 10:29:26 +0100

    Module 9 Ready

---
+++ Module 9/app.py
@@ -0,0 +1,17 @@
+"""
+app.py
+======
+The Transaction Monitoring Dashboard back-end.
+
+Flask does two jobs here:
+  1. It serves the single dashboard page (templates/index.html).
+  2. It exposes a small REST API that returns JSON the page charts itself with.
+
+Run it with:
+
+    python app.py
+
+then open http://127.0.0.1:5000 in a browser.
+"""
+
+# TODO: code me!
